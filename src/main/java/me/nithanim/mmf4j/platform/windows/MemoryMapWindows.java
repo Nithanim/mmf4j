9d9693acbc9eff02d9f13c6b670164da1c551bd5
Nithanim <nithanim@yahoo.de>
2015-08-09 16:46:31 +0200

    Additionally show an error string on windows instead of showing the code alone.

--- src/main/java/me/nithanim/mmf4j/platform/windows/MemoryMapWindows.java
+++ src/main/java/me/nithanim/mmf4j/platform/windows/MemoryMapWindows.java
@@ -6,6 +6,7 @@ import com.sun.jna.platform.win32.Kernel32;
 import com.sun.jna.platform.win32.WinBase;
 import com.sun.jna.platform.win32.WinError;
 import com.sun.jna.platform.win32.WinNT;
+import com.sun.jna.ptr.PointerByReference;
 import java.io.IOException;
 import me.nithanim.mmf4j.MemoryMapBase;
 
@@ -41,7 +42,7 @@ public class MemoryMapWindows extends MemoryMapBase {
             WinNT.FILE_ATTRIBUTE_NORMAL,
             null);
         if (WinNT.INVALID_HANDLE_VALUE.equals(file)) {
-            throw new IOException("Unable to open file: " + Kernel32.INSTANCE.GetLastError());
+            throw new IOException("Unable to open file: " + getLastErrorAsString());
         }
     }
 
@@ -60,7 +61,7 @@ public class MemoryMapWindows extends MemoryMapBase {
             null);
 
         if (mapping == null || WinNT.INVALID_HANDLE_VALUE.equals(mapping)) {
-            throw new IOException("Unable to map file: " + Kernel32.INSTANCE.GetLastError());
+            throw new IOException("Unable to map file: " + getLastErrorAsString());
         } else if (Kernel32.INSTANCE.GetLastError() == WinError.ERROR_ALREADY_EXISTS) {
             //File mapping already existing, TODO how to care about?
             throw new IOException("ERROR_ALREADY_EXISTS, don't know how to handle that!");
@@ -96,7 +97,8 @@ public class MemoryMapWindows extends MemoryMapBase {
 
         boolean resSEOF = MMFKernel32.INSTANCE.SetEndOfFile(file);
         if (!resSEOF) {
-            throw new IOException("Unable to SetEndOfFile: " + Kernel32.INSTANCE.GetLastError());
+            throw new IOException(
+                "Unable to SetEndOfFile: " + getLastErrorAsString());
         }
     }
 
@@ -109,5 +111,42 @@ public class MemoryMapWindows extends MemoryMapBase {
     protected void _close() {
         Kernel32.INSTANCE.CloseHandle(mapping);
         Kernel32.INSTANCE.CloseHandle(file);
+    }
+
+    private static String getLastErrorAsString() {
+        int lastError = Kernel32.INSTANCE.GetLastError();
+        return "LastError: " + lastError + ": " + getErrorString(lastError);
+    }
+
+    private static String getErrorString(int errorcode) {
+        // Thanks to Shog9@http://stackoverflow.com/a/455533/2060704
+        PointerByReference str = new PointerByReference();
+        int ret = Kernel32.INSTANCE.FormatMessage(
+            WinBase.FORMAT_MESSAGE_FROM_SYSTEM | WinBase.FORMAT_MESSAGE_ALLOCATE_BUFFER | WinBase.FORMAT_MESSAGE_IGNORE_INSERTS,
+            Pointer.NULL,
+            errorcode,
+            // Can't use other locales for some reason sometimes. Rust does seem to have the same problem
+            // https://github.com/rust-lang/rust/commit/5e8e1b515a9ef1cd38ee0c71f032415906a7f42d
+            // using system default for now
+            0x0800 /* system default */, // https://msdn.microsoft.com/en-us/library/windows/desktop/dd318693%28v=vs.85%29.aspx
+            str,
+            0,
+            Pointer.NULL);
+        if (ret == 0) {
+            // ret == 15100 when locale was not found.
+            // show the code at least in any case
+            return "(FormatMessage returned with " + Kernel32.INSTANCE.GetLastError() + ", no error string could be fetched)";
+        }
+        if (str.getPointer() != Pointer.NULL) {
+            // Might be extremely wrong for older, unsupported systems according to
+            // http://bytes.com/topic/c/answers/680878-sizeof-tchar
+            // or http://wiki.mcneel.com/developer/sdksamples/countof
+            // TCHAR is either char (1 byte) or wchar_t (2 byte); using wchar_t for now
+            String string = str.getValue().getWideString(0);
+            Kernel32.INSTANCE.LocalFree(str.getValue());
+            return string;
+        } else {
+            throw new RuntimeException("Unable to obtain errorstring for errorcode " + errorcode);
+        }
     }
 }
